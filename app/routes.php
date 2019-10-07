466c9675fc0176380e7c1f29ddb3a454792eac81
guldra-cranch <ymzlgcranch@gmail.com>
2019-10-07 20:47:36 +0900

    routesから配列でレスポンスを返すようにした

--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,12 @@
 $routes = [];
 
 $routes['/']= function (){
+    ob_start();
     include __DIR__.'/../view/index.phtml';
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 $routes['/phpinfo.php'] = function () {
+    ob_start();
     phpinfo();
 };
 return $routes;
