9e4db69a3a858afcbaf65e30c3a3300f3b714bb2
guldra-cranch <ymzlgcranch@gmail.com>
2019-10-07 21:54:35 +0900

    作れた？

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,18 @@
 <?php
-$routes = [];
 
-$routes['/']= function (){
-    ob_start();
-    include __DIR__.'/../view/index.phtml';
-    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
+
+$routes = [];
+$template = new \OreOre\TemplateFactory(__DIR__.'/view/');
+
+$routes['/']= function () use ($template){
+    return [200, ['Content-Type' => 'text/html'], $template->create('index', [
+            'title' => 'ぐーどらのぺーじindex',
+            'name'=>"ぐーどら"
+        ])];
 };
 $routes['/phpinfo.php'] = function () {
     ob_start();
     phpinfo();
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 return $routes;
---
+++ src/Template.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OreOre;
+
+class Template
+{
+    /** ファイルパス **/
+    private $file;
+    /** テンプレートで表示するための変数を保持した配列 **/
+    private $params;
+
+    public function __construct(string $file, array $params)
+    {
+        $this->file = $file;
+        $this->params = $params;
+    }
+
+    public function render()
+    {
+
+        extract($this->params);
+        include $this->file;
+    }
+
+    public function __toString()
+    {
+        ob_start();
+        $this->render();
+        return ob_get_clean();
+    }
+
+}
