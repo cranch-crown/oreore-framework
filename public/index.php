3d1572d47ff51e69d0fc0fd621363dcc3784d23f
guldra-cranch <ymzlgcranch@gmail.com>
2019-10-07 20:15:42 +0900

    routesに記述を移動

--- public/index.php
+++ public/index.php
@@ -2,22 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$route = [];
+$routes = require __DIR__ . '/../app/routes.php';
 
-$route['/']= function (){
-
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-exit;
-};
-$route['/phpinfo.php'] = function () {
-    phpinfo();
-    exit;
-};
 if (isset($_SERVER['REQUEST_URI'])) {
-    $f = $route[$_SERVER['REQUEST_URI']];
+    $f = $routes[$_SERVER['REQUEST_URI']];
     $f();
 } else {
     http_response_code(404);
