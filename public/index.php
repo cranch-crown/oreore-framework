c1496c59b1b8c670b0f3a9bc8734a01e7e8d5a4f
guldra-cranch <ymzlgcranch@gmail.com>
2019-10-07 20:13:09 +0900

    routesファイル作ったよ！

--- public/index.php
+++ public/index.php
@@ -2,7 +2,24 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$route = [];
+
+$route['/']= function (){
+
 echo "<!DOCTYPE html>\n";
 echo "<title>test</title>\n";
 echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
 echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+exit;
+};
+$route['/phpinfo.php'] = function () {
+    phpinfo();
+    exit;
+};
+if (isset($_SERVER['REQUEST_URI'])) {
+    $f = $route[$_SERVER['REQUEST_URI']];
+    $f();
+} else {
+    http_response_code(404);
+    echo'<h1>404 Not Found</h1>';
+}
